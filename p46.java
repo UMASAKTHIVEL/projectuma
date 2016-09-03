d9b65ed400cc169cc002effc7cef6aed49d8ab06
UMASAKTHIVEL <umasakthivel96@gmail.com>
2016-09-03 23:26:44 +0530

    Create p46.java

---
+++ p46.java
@@ -0,0 +1,38 @@
+
+
+import java.util.Scanner;
+
+public class p46{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s;
+		String s1;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextLine();
+		s1=sc.next();
+		StringBuffer a=new StringBuffer(s);
+		StringBuffer b=new StringBuffer(s1);
+		for(int i=0;i<a.length();i++)
+		{
+			for(int j=0;j<b.length();j++)
+			{
+				if(a.charAt(i)==b.charAt(j))
+				{
+					a.deleteCharAt(i);
+				}
+				
+			}
+			System.out.println(a);
+
+						
+		}
+		
+		
+
+	}
+
+}
